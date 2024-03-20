d44ae390c34b464fffc114c93d19446e246ae883
Muhammad Mujtaba Hussain <64016614+mmujtabah@users.noreply.github.com>
2024-03-20 17:39:25 +0500

    Update

--- ChessBoard.h
+++ ChessBoard.h
@@ -1,14 +1,35 @@
 #pragma once++
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+
 
 class ChessBoard
 {
-private: 
-int ** boardData;
-sf::Texture textures[12];
-
+private:
+	int** boardData;
+	unsigned int screenWidth;
+	unsigned int screenHeight;
+	bool endGame;
+	float CellSize;
+	sf::Texture textures[12];
+	sf::RenderWindow* window;
+	sf::VideoMode desktopMode;
+	sf::RectangleShape board[8][8];
+	sf::Event event;
+	sf::Image Icon;
 public:
 	ChessBoard();
-	virtual ~ChessBoard();
-	void drawBoard(sf::RenderWindow& window, sf::RectangleShape board[][8], float cellSize, int screenWidth, int screenHeight);
+	/*void drawBoard(sf::RenderWindow& window, sf::RectangleShape board[][8], float cellSize, int screenWidth, int screenHeight);*/
+	void drawBoard();
 	void drawMatrix();
+	bool WindowIsOpen() const;
+	void pollEvents();
+	bool EndGame() const;
+	void renderWindow();
+	void updateWindow();
+	void initFonts();
+	void initText();
+	void initWindow();
+	virtual ~ChessBoard();
 };
